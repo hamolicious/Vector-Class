f5d33b51bcc2ccfdb77c1d2f2454589349202132
hamolicious <kickfliptv8@gmail.com>
2021-09-17 04:31:39 +0100

    Revert "implemented multi dimensional vector"
    This reverts commit 844bcbd036a94c453825c5ba13888bfe47516d5c.

--- src/vector/__init__.py
+++ src/vector/__init__.py
@@ -1,6 +1,7 @@
 
-from vector.vector import Vector
-from vector.exceptions import DimensionsMismatch
+from vector.vector2d import Vec2d
+from vector.vector3d import Vec3d
+from vector.color import Color
 
 
 
--- src/vector/exceptions.py
+++
@@ -1,7 +0,0 @@
-
-
-class DimensionsMismatch(Exception):
-	pass
-
-
-
